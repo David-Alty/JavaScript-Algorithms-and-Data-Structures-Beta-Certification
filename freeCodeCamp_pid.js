8a89ea15c76e7fd9df43e0c6923ea5296a2da12b
DAVUT_ALTAY <davut.altai@gmail.com>
2024-11-14 16:05:51 +0300

    firstProject

---
+++ freeCodeCamp_pid.js
@@ -0,0 +1,37 @@
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
+
+function getAverage(scores) {
+  let sum = 0;
+
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+
+console.log (getAverage([90,100,70,89,45,67,87]));
+
