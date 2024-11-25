f198e6ac1e1838dbabfc42d6cb08313ed067f3f7
DAVUT_ALTAY <davut.altai@gmail.com>
2024-11-25 10:14:04 +0300

    sort() method

--- 0-Learn Concepts/script.js
+++ 0-Learn Concepts/script.js
@@ -43,4 +43,27 @@ const user = {
 const state = user.address.state; // CA
 
 // Accessing a non-existent nested property with optional chaining
-const zipCode = user.address?.zipCode; // Returns undefined instead of throwing an error
+const zipCode = user.address?.zipCode; // Returns undefined instead of throwing an 
+
+// The sort() method converts elements of an array into strings and 
+// sorts them in place based on their values in the UTF-16 encoding.
+
+const names = ["Tom", "Jessica", "Quincy", "Naomi"];
+names.sort() // ["Jessica", "Naomi", "Quincy", "Tom"]
+
+// The sort() method in JavaScript is used to sort the elements of an array in place (modifies the original array) 
+// returns the sorted array. By default, it sorts the elements as strings in ascending order.
+
+/* 1. Default Sorting Behavior
+If no compareFunction is provided, the sort() method converts the elements to strings and sorts them
+lexicographically (dictionary order based on UTF-16 code points)*/
+
+/*2. Using a Compare Function
+To sort numbers or other complex data types correctly, you need to provide a compare function.
+
+Compare Function Syntax
+The compareFunction takes two arguments, a and b, and determines their order:
+
+If compareFunction(a, b) is less than 0, a comes before b.
+If compareFunction(a, b) is greater than 0, b comes before a.
+If compareFunction(a, b) is 0, their order remains unchang*/
