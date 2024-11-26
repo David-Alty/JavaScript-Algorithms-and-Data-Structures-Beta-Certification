d4edc89a4dd2946b36ce26e390baffccc9e759a5
DAVUT_ALTAY <davut.altai@gmail.com>
2024-11-26 14:01:13 +0300

    JS constructors

---
+++ 0-Learn Concepts/date.js
@@ -0,0 +1,8 @@
+/*In JavaScript, there are many built-in constructors that create objects.
+ A constructor is like a regular function, but starts with a capital letter, and is initialized with the new operator.
+
+For example, you can use the Date() constructor with
+ the new operator to create a new Date object that returns a string with the current date and time:*/
+
+const currentDate = new Date();
+console.log(currentDate);
