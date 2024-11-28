7809f22eb90c46cc3271b4dc2c370044f32cb03e
DAVUT_ALTAY <davut.altai@gmail.com>
2024-11-28 16:34:03 +0300

    localStorage methods

--- 0-Learn Concepts/script.js
+++ 0-Learn Concepts/script.js
@@ -309,3 +309,18 @@ const obj = JSON.parse(jsonString);
 console.log(obj);
 Output: { name: "Alice", age: 25, isStudent: false }
 */
+
+
+const myTaskArr = [
+  { task: "Walk the Dog", date: "22-04-2022" },
+  { task: "Read some books", date: "02-11-2023" },
+  { task: "Watch football", date: "10-08-2021" },
+];
+
+localStorage.setItem("data", JSON.stringify(myTaskArr));
+
+const getTaskArr = localStorage.getItem("data")
+console.log(typeof(getTaskArr))
+
+const getTaskArrObj   = JSON.parse(localStorage.getItem('data'));
+console.log(typeof(getTaskArrObj));
