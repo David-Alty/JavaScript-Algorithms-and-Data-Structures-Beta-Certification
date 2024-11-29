9cc3bc93e9e42d38d706de1141ed25796463f723
DAVUT_ALTAY <davut.altai@gmail.com>
2024-11-29 15:20:47 +0300

    Dec to Bin with while loop

--- 12-Learn Recursion by Building a Decimal to Binary Converter/script.js
+++ 12-Learn Recursion by Building a Decimal to Binary Converter/script.js
@@ -1,3 +1,32 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+
+
+function decToBinary(num) {
+    
+    let decToBin = [];
+    let con = true;
+    const decNUmber = num;
+
+    while(con) {
+        if(num%2 == 0) {
+         decToBin.unshift(0)
+        }
+        else 
+        {
+         decToBin.unshift(1)
+        } 
+        num = Math.floor(num/2);
+     
+        if(num==1) {
+         con=false;
+         decToBin.unshift(1)
+     
+            }
+         }
+   return   `${decNUmber} decimal number is [${decToBin.join(" ")}],[${decToBin.length}] bits in Binary number system.`;
+}
+
+
