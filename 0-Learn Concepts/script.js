8635f9aa8248e552109358a8839ae819de6a017b
DAVUT_ALTAY <davut.altai@gmail.com>
2024-12-02 13:05:33 +0300

    stack data structure

--- 0-Learn Concepts/script.js
+++ 0-Learn Concepts/script.js
@@ -536,4 +536,13 @@ This method is particularly useful for formatting numbers, binary strings, or an
 string.padStart(targetLength ,[padString])
 console.log("42".padStart(6, '123')); // Output: "123142"
 
+/*
+  In computer science, a stack is a data structure where items are stored in a LIFO (last-in-first-out) manner.
+  If you imagine a stack of books, the last book you add to the stack is the first book you can
+  take off the stack. Or an array where you can only .push() and .pop() elements.
+
+  The call stack is a collection of function calls stored in a stack structure. When you call a function, it is added 
+  to the top of the stack, and when it returns, it is removed from the top / end of the stack.
+*/
+
 
