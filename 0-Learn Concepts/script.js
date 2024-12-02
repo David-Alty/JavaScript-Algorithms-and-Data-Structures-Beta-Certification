02a1af6da24ed3536be12604c0e72e867e5c0ba6
DAVUT_ALTAY <davut.altai@gmail.com>
2024-12-02 14:35:00 +0300

    count down function updated

--- 0-Learn Concepts/script.js
+++ 0-Learn Concepts/script.js
@@ -556,10 +556,16 @@ const countdown = (number) => {
   console.log(number);
 
   if (number === 0) {
+    console.log("Reached base case")
     return;
   } else {
     countdown(number - 1);
+    console.log(number);
   }
 };
 
 countdown(3);
+
+/* Now you should see a countdown from 3 to 0, followed by Reached base case, and a count from 1 to 3. This is because, after the recursive loop is finished, the function will continue to execute the code after the recursive call.
+ This is why you see Reached base case before the count from 1 to 3.
+*/
