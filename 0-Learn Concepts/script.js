fabd7780f142da7f3a6016ad576993f82672f552
DAVUT_ALTAY <davut.altai@gmail.com>
2024-12-03 15:46:12 +0300

    capture group ()

--- 0-Learn Concepts/script.js
+++ 0-Learn Concepts/script.js
@@ -737,3 +737,7 @@ const dollarRegex = /[0-9] dollars/i;
 /*
 The dollar value may be more than one digit. To match this, the + quantifier can be used - this matches one or more consecutive occurrences. For example, the regular expression /a+/ matches one or more consecutive a characters. 
 */
+
+/*
+  A capture group is a way to define a part of the expression that should be captured and saved for later reference. You can define a capture group by wrapping a part of your expression in parentheses. For example, /h(i|ey) camper/ would match either hi camper or hey camper, and would capture i or ey in a group.
+*/
