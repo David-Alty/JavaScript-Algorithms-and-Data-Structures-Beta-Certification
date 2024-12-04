40c7bd14d1cd308c45d4046e96ce71011b8a96cf
DAVUT_ALTAY <davut.altai@gmail.com>
2024-12-04 15:12:11 +0300

    default behavior of .sort()

--- 0-Learn Concepts/script.js
+++ 0-Learn Concepts/script.js
@@ -846,3 +846,9 @@ const selectionSort = (array) => {
     }
     return array;
   }
+
+  /*
+    Notice how the number 10 is placed at the beginning of the array. This is because the default behavior of .sort() is to convert the numbers values to strings, and sort them alphabetically. And "10" comes before "2" alphabetically.
+  
+    To fix this, you can pass a callback function to the .sort() method. The callback function has two parameters - for yours, use a and b. The parameters of a and b represent the number values in the array that will be sorted.  
+  */
