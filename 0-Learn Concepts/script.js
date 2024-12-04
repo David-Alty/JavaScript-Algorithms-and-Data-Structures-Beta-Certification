e87fb2aa2a450c9fb2ef03752c56ccef4878128a
DAVUT_ALTAY <davut.altai@gmail.com>
2024-12-04 14:54:16 +0300

    sorting algorithm function added

--- 0-Learn Concepts/script.js
+++ 0-Learn Concepts/script.js
@@ -811,3 +811,38 @@ const bubbleSort = (array) => {
 /*
 Time to implement another sorting algorithm. This time, you'll be implementing a selection sort. Selection sort works by finding the smallest value in the array, then swapping it with the first value in the array. Then, it finds the next smallest value in the array, and swaps it with the second value in the array. It continues iterating through the array until it is completely sorted. 
 */
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(array, array[j], array[minIndex]);
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+  return array;
+}
+
+/*
+  The last sorting algorithm you will implement is the insertion sort. This algorithm works by building up a sorted array at the beginning of the list. It begins the sorted array with the first element. Then it inspects the next element and swaps it backward into the sorted array until it is in a sorted position, and so on.  
+*/
+  const insertionSort = (array) => {
+    for (let i = 1; i < array.length; i++) {
+      const currValue = array[i];
+      let j = i - 1;
+
+      while (j >= 0 && array[j] > currValue) {
+        array[j + 1] = array[j];
+        j--;
+      }
+      array[j + 1] = currValue;
+    }
+    return array;
+  }
