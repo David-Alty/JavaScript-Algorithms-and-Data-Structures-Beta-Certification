22945a639e84b3f903594e302ccaa70eeac937e3
DAVUT_ALTAY <davut.altai@gmail.com>
2024-12-05 16:59:43 +0300

    mode calculate function added

--- 16-Learn Advanced Array Methods by Building a Statistics Calculator/script.js
+++ 16-Learn Advanced Array Methods by Building a Statistics Calculator/script.js
@@ -1,16 +1,32 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
-const testArr1 = [1, 2, 3, 4, 5];
-const testArr2 = [1, 2, 3, 4, 5, 6];
-const isEven = testArr2.length % 2 === 0;
-console.log(isEven);
-const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
-console.log(oddListMedian);
-const evenListMedian = getMean([testArr2[testArr2.length / 2 - 1], testArr2[testArr2.length / 2]]);
-console.log(evenListMedian);
 const getMedian = (array) => {
-  const sorted = array.sort((a, b) => a - b);
+  const sorted = array.toSorted((a, b) => a - b);
+  const median =
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
+  return median;
 }
+
+const getMode = (array) => {
+  const counts = {};
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  })
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
+}
+
+
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -18,6 +34,10 @@ const calculate = () => {
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
   
   const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  const mode = getMode(numbers);
 
   document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
 }
