3229ea287042f91e68af94542e2ace65dfc0e55a
DAVUT_ALTAY <davut.altai@gmail.com>
2024-12-12 10:15:22 +0300

    EventListener with a callback function

--- 0-Learn Concepts/script.js
+++ 0-Learn Concepts/script.js
@@ -1452,3 +1452,16 @@ person1.greet(); // Output: Hello, my name is Alice and I am 25 years old.
 
  ); 
  // we can use spread operator to iterate into a collection and convert them to an array.
+
+/*  
+Add your callback function to the forEach method. It should take a btn parameter. Then, in the callback, add an event listener to the btn. The event listener should listen for a click event, and should take another callback with an event parameter. The second callback should be empty. 
+*/
+
+ [...addToCartBtns].forEach(
+  (btn) => {
+    btn.addEventListener("click", (event) => {
+
+    })
+  }
+);
+
