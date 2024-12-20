f5d5a852c1aa0cb6a2f61e12345d59e7134fe0cd
DAVUT_ALTAY <davut.altai@gmail.com>
2024-12-20 11:08:59 +0300

    eventListener added to purchaseBtn

--- 24-Build a Cash Register Project/script.js
+++ 24-Build a Cash Register Project/script.js
@@ -1,4 +1,4 @@
-let price = 1.87;
+let price = 1.87; 
 let cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
@@ -10,3 +10,29 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
+
+const inputCash = document.getElementById("cash"); 
+const change = document.getElementById("change-due"); 
+const purchaseBtn = document.getElementById("purchase-btn"); 
+
+let currentUnits = [
+  ['PENNY', 0.01],
+  ['NICKEL', 0.05],
+  ['DIME', 0.1],
+  ['QUARTER', 0.25],
+  ['ONE', 1],
+  ['FIVE', 5],
+  ['TEN', 10],
+  ['TWENTY', 20],
+  ['ONE HUNDRED', 100]
+];
+
+purchaseBtn.addEventListener("click", () => {
+  const inputCashValue = parseFloat(inputCash.value);
+  const changeDue = inputCashValue - price;
+
+  if (inputCashValue < price) {
+    alert("Customer does not have enough money to purchase the item")
+  }
+  
+});
